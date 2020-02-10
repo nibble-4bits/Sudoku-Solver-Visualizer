69a4929abbe641333bdcfb1713eeb18c48d2f964
nibble4bits <andiyasha16@gmail.com>
2020-02-10 00:08:39 -0600

    main.js created for Node entry point

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App started and listening on port ${PORT}!`);
+});
