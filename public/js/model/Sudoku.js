aa84842295c5c40971846a15f30cdf189cbe41c7
Luis De Anda <ldeandac16@gmail.com>
2020-02-16 21:16:37 -0600

    Created Sudoku class to manage sudoku model

---
+++ public/js/model/Sudoku.js
@@ -0,0 +1,102 @@
+'use strict';
+
+class Sudoku {
+    constructor() {
+        this.board = [
+            [0, 2, 0, 0, 0, 0, 0, 0, 7],
+            [4, 0, 0, 0, 0, 5, 0, 8, 0],
+            [0, 3, 0, 0, 9, 0, 0, 0, 0],
+            [5, 0, 0, 0, 0, 8, 0, 7, 4],
+            [0, 0, 4, 2, 0, 0, 1, 0, 0],
+            [0, 6, 0, 0, 0, 9, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0, 0, 0],
+            [7, 0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 3, 8, 0, 6, 0, 0]
+        ];
+    }
+
+    /**
+     * Clears the board
+     */
+    clear() {
+        this.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+    }
+
+    /**
+     * Checks if a number can be placed in the square specified by (row, col)
+     * @param {Number} row Row index to be checked
+     * @param {Number} col Column index to be checked
+     * @param {Number} number Number to test if it is possible to place
+     * @returns {Boolean} True if the number can be placed, false otherwise
+     */
+    isNumberValid(row, col, number) {
+        return this.isNumberValidRow(row, number) &&
+            this.isNumberValidCol(col, number) &&
+            this.isNumberValidBox(row, col, number);
+    }
+
+    /**
+     * Checks if a number can be placed in the row specified
+     * @param {Number} row Row index to be checked
+     * @param {Number} number Number to test if it is possible to place
+     * @returns {Boolean} True if the number can be placed, false otherwise
+     */
+    isNumberValidRow(row, number) {
+        for (let y = 0; y < this.board.length; y++) {
+            if (this.board[row][y] === number) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks if a number can be placed in the column specified
+     * @param {*} col Column index to be checked
+     * @param {*} number Number to test if it is possible to place
+     * @returns {Boolean} True if the number can be placed, false otherwise
+     */
+    isNumberValidCol(col, number) {
+        for (let x = 0; x < this.board.length; x++) {
+            if (this.board[x][col] === number) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks if number can be placed in the box that contains the square specified by (row, col)
+     * @param {Number} row Row index to be checked
+     * @param {Number} col Column index to be checked
+     * @param {Number} number Number to test if it is possible to place
+     * @returns {Boolean} True if the number can be placed, false otherwise
+     */
+    isNumberValidBox(row, col, number) {
+        let rowStart = row - row % 3;
+        let rowEnd = rowStart + 3;
+        let colStart = col - col % 3;
+        let colEnd = colStart + 3;
+
+        for (let x = rowStart; x < rowEnd; x++) {
+            for (let y = colStart; y < colEnd; y++) {
+                if (this.board[x][y] === number) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+export { Sudoku };
